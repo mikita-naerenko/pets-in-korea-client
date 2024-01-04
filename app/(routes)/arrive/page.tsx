b88cb3ce78e1d2451f182cf13df876cea0a7b1d9
Mikita <tetropak555666@gmail.com>
2024-01-04 18:34:56 +0900

    arr/dep

--- app/(routes)/arrive/page.tsx
+++ app/(routes)/arrive/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import getArticles from "@/actions/get-articles";
 import getTags from "@/actions/get-tags";
-import Arrive from "@/components/departure";
+import Arrive from "@/components/arrive";
 
 export default async function Page() {
   const tags = await getTags();
