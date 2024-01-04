f4422b99df75d836b7e78ff9e84fbe3a6ec83d35
Mikita <tetropak555666@gmail.com>
2024-01-04 19:56:49 +0900

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,7 @@ export default function RootLayout({
       <ThemeProvider theme={theme}>
         <body className={font.className} style={{ position: "relative" }}>
           <Container maxWidth={"xl"}>
-            {/* <Header> */}
-            {children}
-            {/* </Header> */}
+            <Header>{children}</Header>
             <UpToTopButton />
             <Footer />
           </Container>
--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
     images: {
         domains: ["res.cloudinary.com"]
-      }
+      },
+      reactStrictMode: false,
 }
 
 module.exports = nextConfig
