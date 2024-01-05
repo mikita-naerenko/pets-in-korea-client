5cd4f20678c1030b37f2253b60ab20372769becc
Mikita <tetropak555666@gmail.com>
2024-01-06 00:52:58 +0900

    fix

--- app/(routes)/[singleArticleId]/page.tsx
+++ app/(routes)/[singleArticleId]/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import getArticle from "@/actions/get-article";
 import SingleArticle from "@/components/single-article";
 import { getBlurData } from "@/lib/getBlurData";
@@ -11,10 +11,18 @@ export default async function page({
   const articleId = params.singleArticleId;
   const article = await getArticle({ id: articleId });
   const imageURL = article?.images?.[0].url;
+  console.log(article);
 
   const blurData = imageURL && (await getBlurData(imageURL));
 
   return (
-    <SingleArticle article={article} blurImage={blurData ? blurData : null} />
+    <>
+      <Suspense fallback={<div>Loading</div>}>
+        <SingleArticle
+          article={article}
+          blurImage={blurData ? blurData : null}
+        />
+      </Suspense>
+    </>
   );
 }
