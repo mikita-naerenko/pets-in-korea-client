4b1203376ddc2922ea4b7753144083afe7052700
Mikita <tetropak555666@gmail.com>
2024-01-06 19:04:33 +0900

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 // @ts-check
-import withPlaiceholder from "@plaiceholder/next";
+// import withPlaiceholder from "@plaiceholder/next";
+
 
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  transpilePackages: ["@plaiceholder/ui"],
   images: {
         domains: ["res.cloudinary.com"],
       },
-      reactStrictMode: false,
 };
 
-export default withPlaiceholder(nextConfig);
+export default nextConfig;
 
 
 // /**
