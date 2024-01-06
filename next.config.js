172809662cb69db840a038f337bd24175cbff5f8
Mikita <tetropak555666@gmail.com>
2024-01-06 13:06:44 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,21 +1,31 @@
 // @ts-check
-
+import withPlaiceholder from "@plaiceholder/next";
 
 /**
  * @type {import('next').NextConfig}
  */
-
-const nextConfig = {
-  // transpilePackages: ["@plaiceholder/ui"],
+const config = {
+  transpilePackages: ["@plaiceholder/ui"],
   images: {
-    domains: ["res.cloudinary.com"],
+    domains: ["images.unsplash.com"],
   },
-  reactStrictMode: false,
 };
 
-// export default withPlaiceholder(nextConfig);
+export default withPlaiceholder(config);
 
-module.exports = nextConfig;
+
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+
+// const nextConfig = {
+//   images: {
+//     domains: ["res.cloudinary.com"],
+//   },
+//   reactStrictMode: false,
+// };
+
+// module.exports = nextConfig;
 
 // git add .
 // git commit -m 'test'
