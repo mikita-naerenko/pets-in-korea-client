750aedccae38c5bc161f018256801bab2566740b
Mikita <tetropak555666@gmail.com>
2024-02-13 17:23:01 +0900

    fix

--- tests/header.spec.ts
+++ tests/header.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test("search-component", async ({ page }) => {
   //   const searchButton = page.locator('[data-testid="search-button"]');
-  await page.goto("http://localhost:3001/");
+  await page.goto("https://pets-in-korea.com/");
   await page.getByTestId("search-button").click();
   await expect(page.getByTestId("search-button-icon")).not.toBeVisible();
   await expect(page.getByTestId("close-search-button-icon")).toBeVisible();
