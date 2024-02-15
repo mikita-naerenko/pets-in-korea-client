37226d02a47abf37b9127c1a0e98b4377a15e9b4
Mikita <tetropak555666@gmail.com>
2024-02-15 20:26:48 +0900

    fix

--- lib/getBlurData.tsx
+++ lib/getBlurData.tsx
@@ -1,13 +1,13 @@
-import { getPlaiceholder } from "plaiceholder";
+// import { getPlaiceholder } from "plaiceholder";
 
-export const getBlurData = async (url: string) => {
-  try {
-    const buffer = await fetch(url).then(async (res) =>
-      Buffer.from(await res.arrayBuffer())
-    );
-    const { base64 } = await getPlaiceholder(buffer);
-    return base64;
-  } catch (e) {
-    console.error(e);
-  }
-};
+// export const getBlurData = async (url: string) => {
+//   try {
+//     const buffer = await fetch(url).then(async (res) =>
+//       Buffer.from(await res.arrayBuffer())
+//     );
+//     const { base64 } = await getPlaiceholder(buffer);
+//     return base64;
+//   } catch (e) {
+//     console.error(e);
+//   }
+// };
