5e2de49e61ff8dadec46d6bb00250ab46ac1b4a0
Mikita <tetropak555666@gmail.com>
2024-02-16 17:00:57 +0900

    ssg dictionary

---
+++ app/(routes)/dictionary/[slug]/page.tsx
@@ -0,0 +1,45 @@
+import getTheme from "@/actions/get-theme";
+import getThemes from "@/actions/get-themes";
+import SingleTheme from "@/components/single-theme";
+import BasicBreadcrumbs from "@/components/ui/basic-breadcrumbs/basic-breadcrumbs";
+import { Theme } from "@/lib/type";
+
+import { Metadata, ResolvingMetadata } from "next";
+
+export async function generateStaticParams() {
+  const themes = await getThemes();
+
+  return themes.map((item) => ({
+    slug: item.id,
+  }));
+}
+
+// export async function generateMetadata(
+//   {
+//     params,
+//   }: {
+//     params: { themeId: string };
+//   },
+//   parent: ResolvingMetadata
+// ): Promise<Metadata> {
+//   const theme = await getTheme({ id: params.themeId });
+
+//   return {
+//     title: theme.label,
+//     description: `Русско-корейский разговорник на тему ${theme.label}`,
+//     openGraph: {
+//       title: theme.label,
+//     },
+//   };
+// }
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const theme = await getTheme({ id: params.slug });
+
+  return (
+    <>
+      <BasicBreadcrumbs currentPage={theme.rusLabel} />
+      <SingleTheme theme={theme} />
+    </>
+  );
+}
