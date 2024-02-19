e23cc3c1eb27a424532cff1326a42624eeed3c2f
Mikita <tetropak555666@gmail.com>
2024-02-19 15:48:34 +0900

    smth

--- components/main-navigation-buttons/index.tsx
+++ components/main-navigation-buttons/index.tsx
@@ -41,7 +41,7 @@ export default function MainNavigationButtons() {
                 quality={IMAGE_QUALITY.mainNavigationButtons}
                 fill
                 style={styleImage}
-                priority
+                priority={true}
               />
             </ImageContainer>
             <ImageBackdrop className="MuiImageBackdrop-root" />
